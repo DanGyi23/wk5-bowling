d0d24faaca1628bdd24f8a18510e5cc7ce4cc086
Dan Gyi <daniel.gyi@gmail.com>
2019-09-21 07:04:02 +0100

    scorecard kind of working

--- interface.js
+++ interface.js
@@ -1,51 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
   scorecard = new Scorecard;
   player1 = new Player;
   player2 = new Player;
 
-  
+
   frameScoreInsert = function () {
     for (i = 1; i <= 2; i++) {
       if (i == 1) {
-        for (j = 1; j <= player1._array.length; j++) { 
+        for (j = 1; j <= player1._array.length; j++) {
           $('#p' + i.toString() + 'f' + j.toString()).text((player1._array[j - 1]))
         }
-       } else {
-        for (k = 1; k <= player2._array.length; k++) { 
+      } else {
+        for (k = 1; k <= player2._array.length; k++) {
           $('#p' + i.toString() + 'f' + k.toString()).text((player2._array[k - 1]))
         };
       };
-  }
-
-  // need to remove the / and X from this method
-  }
-
-    totalScoreInsert = function() {
-      var total1 = 0;
-      var total2 = 0;
-      for (i = 0; i < player1._array.length; i++) {
-        for (var j = 0; j < player1._array[i].length; j++) {
-          total1 += player1._array[i][j];
-        }
-      }
-      for (k = 0; k < player2._array.length; k++) {
-        for (var l = 0; l < player2._array[k].length; l++) {
-          total2 += player2._array[k][l];
-        }
-      }
-
-      // can look to call the cumulative scorer here and delete function
-      
-      
-      
     }
+  }
 
-  $('#score-input').submit(function(event){
+  $('#score-input').submit(function (event) {
     event.preventDefault();
     var player = $('#player-selected').val();
     var score1 = parseInt($('#score1-input').val());
     var score2 = parseInt($('#score2-input').val());
-    
+
     if (score1 + score2 > 10) {
       $('#error-message').text('Maximum score of 10 allowed')
     } else {
@@ -60,7 +38,7 @@ $(document).ready(function(){
         var total2 = scorecard.scoreTracker(player2, player2._array.length)
         $('#p2total').text(total2)
       };
-      
+
     };
   });
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,71 +1,74 @@
-Scorecard = function() {
+Scorecard = function () {
 
 
   Scorecard.prototype.frameAdd = function (player, score1, score2) {
-    if (score1 + score2 != 10) {
-      if (player._array.length < 10) {
+    if (player._array.length < 9) {
+      if (score1 + score2 != 10) {
         player._array.push([score1, score2]);
       }
-    };
 
-    if (score1 + score2 == 10 && score1 != 10 && score2 != 10) {
+
+      if (score1 + score2 == 10 && score1 != 10) {
         player._array.push([score1, " /"])
-    };
+      };
 
-    if (score1 == 10) {
+      if (score1 == 10) {
         player._array.push(["X"])
+      };
     };
   }
 
-  Scorecard.prototype.scoreTracker = function (player, index) {
-    for (i = 0; i < index ; i++) {
-      console.log(i)
-      for (j = 0; j < player._array[i].length; j++) {
-        if (i == 0) {
-          // need to add strike or spare cond inside this if sttment.
-          player._currentscore += player._array[i][j]
-        } else {
-        // strike followed by strike
-        if (player._array[(i - 1)].includes("X") && player._array[i].includes("X")) {
+  Scorecard.prototype.scoreTracker = function (player, i) {
+    if (i == 1) {
+      if (player._array[i - 1].includes("X") || player._array[i - 1].includes(" /")) {
+        player._currentscore += 10
+      } else {
+        player._currentscore += player._array[i - 1][j]
+      };
+    };
 
+    if (i > 1 && i < 9) {
+      for (j = 0; j < player._array[i - 1].length; j++) {
+
+        // strike followed by strike
+        if (player._array[(i - 2)].includes("X") && player._array[i - 1].includes("X")) {
           player._currentscore += 20
-        } 
+        }
         // strike followed by spare
-        else if (player._array[i - 1].includes("X") && player._array[i].includes(" /")) {
+        else if (player._array[i - 2].includes("X") && player._array[i - 1].includes(" /")) {
           player._currentscore += 20
         }
         // strike followed by normal
-        else if (player._array[i - 1].includes("X") && !player._array[i].includes(" /") && !player._array[i].includes("X")) {
-          player._currentscore + player._array[i][j] * 2
-        } 
+        else if (player._array[i - 2].includes("X") && !player._array[i - 1].includes(" /") && !player._array[i - 1].includes("X")) {
+          player._currentscore += player._array[i][j] * 2
+        }
         // spare followed by strike
-        else if (player._array[i - 1].includes(" /") && player._array[i].includes("X")) {
+        else if (player._array[i - 2].includes(" /") && player._array[i - 1].includes("X")) {
           player._currentscore += 20
         }
         // spare followed by spare
-        else if (player._array[i - 1].includes(" /") && player._array[i].includes(" /")) {
+        else if (player._array[i - 2].includes(" /") && player._array[i - 1].includes(" /")) {
           player._currentscore += 20
         }
         // spare followed by normal
-        else if (player._array[i - 1].includes(" /") && !player._array[i].includes(" /") && !player._array[i].includes("X")) {
-          player._currentscore + player._array[i][0] * 2
+        else if (player._array[i - 2].includes(" /") && !player._array[i - 1].includes(" /") && !player._array[i - 1].includes("X")) {
+          player._currentscore += player._array[i][0] * 2
         }
         // normal followed by strike
-        else if (!player._array[i -1].includes(" /") && !player._array[i - 1].includes("X") && player._array[i].includes("X")) {
+        else if (!player._array[i - 2].includes(" /") && !player._array[i - 2].includes("X") && player._array[i - 1].includes("X")) {
           player._currentscore += 10
         }
         // normal followed by spare
-        else if (!player._array[i - 1].includes(" /") && !player._array[i - 1].includes("X") && player._array[i].includes("/")) {
+        else if (!player._array[i - 2].includes(" /") && !player._array[i - 2].includes("X") && player._array[i - 1].includes("/")) {
           player._currentscore += 10
         }
         // normal followed by normal
-        else if (!player._array[i - 1].includes(" /") && !player._array[i - 1].includes("X") && !player._array[i].includes(" /") && !player._array[i].includes("X")) {
-          player._currentscore + player._array[i][j]
+        else if (!player._array[i - 2].includes(" /") && !player._array[i - 2].includes("X") && !player._array[i - 1].includes(" /") && !player._array[i - 1].includes("X")) {
+          player._currentscore += player._array[i][j]
         }
 
+      }
     }
-  }
-  }
     return player._currentscore
   }
 
