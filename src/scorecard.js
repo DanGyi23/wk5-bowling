7de620f441ef3f44adfe033cf3a63be2402425a3
Dan Gyi <daniel.gyi@gmail.com>
2019-09-22 19:22:02 +0100

    partial frame10 solution

--- src/scorecard.js
+++ src/scorecard.js
@@ -78,10 +78,53 @@ Scorecard = function () {
   }
 
   Scorecard.prototype.frameTenAdd = function(player, score1, score2, score3) {
+    if (player._array.length == 9) {
+      if (score1 + score2 < 10) {
+        player._array.push([score1, score2]);
+      }
 
+      if (score1 + score2 == 10 && score1 != 10) {
+        player._array.push([score1, " /", score3])
+      };
+
+      if (score1 == 10) {
+        if (score2 + score3 == 10) {
+          player._array.push(["X", score2, " /"])
+        } else if (score2 == 10 && score3 != 10) {
+          player._array.push(["X", "X", score3])
+        } else if (score2 == 10 && score3 == 10) {
+          player._array.push(["X", "X", "X"])
+        };
+      };
+
+    };
   }
 
-  Scorecard.prototype.frameTenCalc = function(player) {
+  Scorecard.prototype.frameTenCalc = function(player, i) {
+    if (i == 10) {
+      if (player._array[(i - 3)].includes("X") && player._array[i - 2].includes("X")) {
+    // st st
+    
+    // normal (score 1x3 + score 2x2)
+    // spare then normal (20 + score1 + score3)
+    // spare then strike (30 + score 2)
+    // strike then normal (30 + 2x score 2 + score 3)
+    // strike then spare (40 + score 2)
+    // strike strike normal (50 + normal)
+    // strike strike strike (60)
+      } else if (player._array[(i - 3)].includes("X") && player._array[i - 2].includes(" /")) {
+        // st sp
+      } else if (!player._array[(i - 3)].includes("X") && player._array[i - 2].includes("X")) {
+        //norm st
+        //sp st
+      } else if (player._array[(i - 2)].includes(" /") || (player._array[(i - 3)].includes("X") && !player._array[i - 2].includes(" /") && !player._array[i - 2].includes("X"))) {
+        // norm sp
+        // sp sp
+        // st norm
+      } else if (!player._array[(i - 3)].includes(" /") && !player._array[(i - 3)].includes("X") && player._array[i - 2].includes(" /") && player._array[i - 2].includes("X")) {
+        //norm norm
+      }
+    }
 
   }
 
